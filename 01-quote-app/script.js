cc05f25a4247fb8648376eee0cc9ddad1aec37b8
Lorenz Kuehl <lorenz.kuehl.coding@gmail.com>
2024-02-08 14:52:32 +0100

    including state, render and catch

--- 01-quote-app/script.js
+++ 01-quote-app/script.js
@@ -1,12 +1,35 @@
+//Quote App
+
+//Variablen
 const quoteBtn = document.getElementById("get-quote");
 const quote = document.getElementById("quote");
 const author = document.getElementById("author");
 
+//Initial State
+let state = {
+  quote: "Dies das Ananas",
+  author: "Paulo Coelho",
+};
+
+//Render function
+function render() {
+  quote.innerText = state.quote;
+  author.innerText = `- ${state.author}`;
+}
+
+//Eventlistener
 quoteBtn.addEventListener("click", function loadQuote() {
   fetch("https://dummy-apis.netlify.app/api/quote")
     .then((response) => response.json())
     .then((data) => {
-      quote.textContent = data.quote;
-      author.textContent = `- ${data.author}`;
+      state.quote = data.quote;
+      state.author = data.author;
+    })
+    .catch((error) => {
+      console.log(error);
     });
+  render();
 });
+
+//Initial render call
+render();
